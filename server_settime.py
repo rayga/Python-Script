a9e50e3ed5f1417f9abb993e942b87c57693575a
rayga <arga@lahardi.com>
2016-06-03 15:46:00 +0700

    Create server_settime.py

---
+++ server_settime.py
@@ -0,0 +1,14 @@
+__author__ = 'n4rut0'
+
+import socket
+
+hostname = raw_input("Enter hostname : ")
+port = int(raw_input("Enter port number : "))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((hostname, port))
+s.settimeout(5) #Jika tertulis 5, berarti durasinya adalah 5 Detik
+data, addr = s.recvfrom(1024)
+print "Received from ", addr
+print "Obtained ", data, " Bytes"
+
