ca1a37493dcd8e3ae7d683ee028a1b628ed8e48f
Rayga Lightscene <arga@lahardi.com>
2016-06-07 13:18:57 +0700

    Update sniffing.py

--- sniffing.py
+++ sniffing.py
@@ -1,6 +1,17 @@
 import struct
+import socket
 
-ms = struct.pack('hhl', 1,2)
-print (ms)
-k = struct.unpack('hhl',ms)
-print k
+hostname = raw_input("Enter hostname target : ")
+port = int(raw_input("Enter port number : ")
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((hostname,port))
+s.listen(1)
+conn, addr = s.accept()
+
+print "Connected by ", addr
+msz = struct.pack('hhl',1,2)
+conn.send(msz)
+conn.close()
+
+
