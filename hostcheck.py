854db4a341b5e09aa2602e8f00c00447b200de1d
Rayga Lightscene <arga@lahardi.com>
2016-06-21 13:14:20 +0700

    Update hostcheck.py

--- hostcheck.py
+++ hostcheck.py
@@ -36,3 +36,34 @@ for ip in xrange(st1, en1):
 t2 = datetime.now()
 total = t2 - t1
 print "Scanning complete in ", total
+
+####Revisi####
+
+import os
+import platform
+from datetime import datetime
+
+ip = raw_input("[*]Enter network address : ")
+start = raw_input("[*]Enter start address : ")
+end = raw_input("[*]Enter end address : ")
+end = end + 1
+os = platform.system()
+split = ip.split(".")
+a = "."
+net = split[0] + a + split[1] + a + split[2]
+ping = "ping -n 1 "
+
+t1 = datetime.now()
+print "Scanning host...."
+for p in xrange(start,end):
+  link = net + p
+  req = ping + link
+  res = os.popen(req)
+  for opt in res.readlines():
+    if opt.count("TTL"):
+      print link, " : LIVE"
+  
+t2 = datetime.now()
+total = t2 - t1
+print "Scanning completed in % " %(total)
+
