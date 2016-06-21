e28a6998e49f22665d98b64ca88a9bffc031d637
Rayga Lightscene <arga@lahardi.com>
2016-06-21 13:39:03 +0700

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -15,3 +15,36 @@ for portlist in range(min_port,max_port):
     s.close()
     
 sys.exit()
+
+####Update terbaru####
+
+##########
+import socket
+import sys
+from datetime import datetime
+
+ip = raw_input("[*]Enter hostname target : ")
+min = 1
+max = 1150
+print"Scanning proses...."
+t1 = datetime.now()
+
+try:
+    for port in xrange(min,max):
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        result = s.connect_ex((ip,port))
+        if (result == 0):
+            print port, "[ ",socket.getservbyport(port),"] : OPEN"
+        s.close()
+except KeyboardInterrupt:
+    print "User interrupt keyboard"
+    sock.close()
+    t2 = datetime.now()
+    total = t2 - t1
+    print "Scanning completed in ",total
+
+t2 = datetime.now()
+total = t2 - t1
+print "Scanning completed in ",total
+sys.exit()
+
