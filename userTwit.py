dde9b45eb12297e06cc57ccbd1dd256ee3ab2161
Rayga Lightscene <arga@lahardi.com>
2016-11-29 16:02:39 +0700

    Update userTwit.py

--- userTwit.py
+++ userTwit.py
@@ -12,17 +12,18 @@ api = tweepy.API(auth)
 
 list = open('/home/queen/Downloads/list.txt','w')
 if(api.verify_credentials):
-	print 'We Successfully logged in.'
-	user = tweepy.Cursor(api.followers, screen_name="detikcom").items()
-	while True:
-		try:
-			u = next(user)
-			list.write(u.screen_name + ' \n')
-		except:
-			time.sleep(15*60)
+    print 'We Successfully logged in.'
+user = tweepy.Cursor(api.followers, screen_name="detikcom").items()
+	
+while True:
+    try:
+	u = next(user)
+	list.write(u.screen_name + ' \n')
+		
+    except:
+	time.sleep(15*60)
 
-print 'We got a timeout ...  Sleeping for 15 minutes'
-
-u = next(user)
-list.write(u.screen_name + ' \n')
+ 	print 'We got a timeout ...  Sleeping for 15 minutes'
+	u = next(user)
+	list.write(u.screen_name + ' \n')
 list.close()
